a815073853f7fa0e878230d06d4b7a631c273352
Taller de R <edfemagonza@gmail.com>
2022-10-26 14:16:56 -0500

    upate repo

--- source/clase-10.R
+++ source/clase-10.R
@@ -4,48 +4,39 @@
 # R version 4.1.1 (2021-08-10)
 #===========================================#
 
-## initial configuration
+## **[0.] Configuración inicial**
+
+#### **☑ entorno**
 rm(list=ls()) # limpiar entorno
 set.seed(12345) # fijar semilla
 
-## load packages
+#### **☑ Librerias:**
+
+## llamar&instalr librerias de la clase
 require(pacman)
-p_load(tidyverse , rio , data.table , png , grid)
+p_load(tidyverse , rio , data.table)
 
-## Hoy veremos
+## **[1.] Funciones**
+browseURL("https://fhernanb.github.io/Manual-de-R/creafun.html")
+browseURL("https://es.r4ds.hadley.nz/funciones.html") 
+browseURL("https://www.r-bloggers.com/2016/02/functions-exercises/")
+browseURL("https://adv-r.hadley.nz/conditions.html")
 
-### **1.** Funciones
+### **1.1 Estructura de una función**
 
-### **2.** Familia apply
-
-#======================#
-#====== Funciones =====#
-#======================#
-
-browseURL(url = "https://fhernanb.github.io/Manual-de-R/creafun.html", browser = getOption("browser")) # partes de una función
-browseURL(url = "https://es.r4ds.hadley.nz/funciones.html", browser = getOption("browser")) # cuando se debería escribir una función
-browseURL(url = "https://www.r-bloggers.com/2016/02/functions-exercises/", browser = getOption("browser")) # ejercicios extras
-browseURL(url = "https://adv-r.hadley.nz/conditions.html", browser = getOption("browser")) # errors, messages and warnings
-
-#----------------------#
-## Funciones
-dev.off()
-grid.raster(readPNG("input/pics/function_machine.png")) # source: https://fhernanb.github.io
-
-#----------------------#
-## Veamos un ejemplo (...)
-cat("message, warnings & errors indican diferentes niveles de mensajes. 
-     *message: informa resultados... etc. 
-     *warnings: informa algun tipo de error o anuncio importante que no impide el funcionamiento del programa.
-     *error: indicador sobre el fua = ncionamiento correcto de la funcion.")
+## message, warnings & errors indican diferentes niveles de mensajes:
   
+## **message:** informa resultados... etc. 
+## **warnings:** informa algun tipo de error o anuncio importante que no impide el funcionamiento del programa.
+## **error:** indicador sobre el fua = ncionamiento correcto de la funcion.
+
+## Ejemplo
 unir <- function(x, y){
         palabra = paste0(x," - ",y) %>% toupper()
         message(paste("message: las combinacion de las palabras producen:", palabra))
 }
 unir(x = "hola", y = "clase")
 
-#----------------------#
 ## Veamos otro ejemplo (...)
 remove_na <- function(x) x = ifelse(is.na(x)==T,0,x)
 
@@ -55,54 +46,49 @@ vector
 vector <- remove_na(x = vector)
 vector
 
+## Veamos otro ejemplo (...)
 storms %>% head()
-df <- storms %>% as_tibble() 
-df$tropicalstorm_force_diameter
-remove_na(x = df$tropicalstorm_force_diameter) %>% print()
 
-df = df %>% mutate(tropicalstorm_force_diameter = remove_na(tropicalstorm_force_diameter))
-df %>% head()
+df <- storms %>% select(name,year,tropicalstorm_force_diameter) 
 
-#----------------------#
-## Veamos un ejemplo (...)
-cat("funcion que regresa el producto de un numero por si mismo")
+## 6509 NA
+summary(df$tropicalstorm_force_diameter)
+
+## replace NA por 0
+df <- df %>% 
+      mutate(tropicalstorm_force_diameter = remove_na(tropicalstorm_force_diameter))
+
+summary(df$tropicalstorm_force_diameter)
+
+## Veamos otro ejemplo: funcion que regresa el producto de un numero por si mismo
 num_2 <- function(x){
-        c = x*x
-return(c)
+         c = x*x
+         return(c)
 }
 num_2(x = 4)
 num_2(x = "A")
 
-# incluir controles de flujo
+## incluir controles de flujo
 num_2 <- function(numero){
-  
-        # si es un numero
-        if (is.numeric(numero)){
-            c = numero*numero
-        return(c)
-        }
         
-        # si no es un numero
-        if (is.numeric(numero)==F){
-            warning(paste0(numero," no es un número"))
-        }
+         # si es un numero
+         if (is.numeric(numero)){
+             c = numero*numero
+         return(c)
+         }
+        
+         # si no es un numero
+         if (is.numeric(numero)==F){
+         warning(paste0(numero," no es un número"))
+         }
 }
 num_2(numero = 10)
 num_2(numero = "hola")
 num_2(numero = "10")
 
-#======================#
-#=== Familia apply ====#
-#======================#
+## **[2.] Apply, Lapply & Sappl**
 
-#### Apply, Lapply & Sapply 
-dev.off()
-grid.raster(readPNG("input/pics/familia_apply.png"))
-
-#----------------------#
-## Apply
-dev.off()
-grid.raster(readPNG("input/pics/apply.png"))
+### **2.1 Apply**
 
 ####  Operaciones por columnas
 mtcars
@@ -112,11 +98,9 @@ apply(mtcars , 2 , function(columna) min(...=columna , na.rm=T))
 ####  Operaciones por filas
 apply(X = mtcars, MARGIN = 1, function(x) sum(x))
 
-#----------------------#
-## Lapply
-dev.off()
-grid.raster(readPNG("input/pics/lapply.png"))
+### **2.2 Lapply**
 
+## aplicar sobre dataframe
 lapply(mtcars, function(x) summary(x))
 lap <- lapply(mtcars, function(x) summary(x))
 lap
@@ -126,35 +110,26 @@ table(is.na(storms$hu_diameter))
 table(is.na(storms$ts_diameter))
 lapply(storms ,function(x)  table(is.na(x)))
 
-#----------------------#
-## Sapply
-dev.off()
-grid.raster(readPNG("input/pics/sapply.png"))
-
+### **2.3 Sapply**
 sap <- sapply(mtcars, summary)
 sap
 
-#=========================#
-#==== Aplicación: chip ===#
-#=========================#
+## **[3.] Aplicación: chip**
 
-# limpiar entorno
+## limpiar entorno
 rm(list=ls()) 
 
-#----------------------#
 ## Chip
 browseURL("https://www.chip.gov.co/schip_rt/index.jsf")
 
 ejemplo <- import("input/chip/2019/11767600044K212410-1220191625694914330.xls", skip = 7) %>% as_tibble()
 ejemplo
 
-#----------------------#
 ## 1. Obtener rutas de los datos
 list.files("input/chip",full.names=T, recursive = T) 
 
 paths <- list.files("input/chip",full.names=T, recursive = TRUE) %>% unlist()
 
-#----------------------#
 ## 2. Hacer ejemplo para una observacion
 
 ## 2.1. leer archivo
@@ -174,28 +149,25 @@ paths <- list.files("input/chip",full.names=T, recursive = TRUE) %>% unlist()
 
 #----------------------#
 ## 3. Generalizar ejemplo en una función
-f_extrac = function(path,tipo_rubro){
+f_extrac <- function(path,tipo_rubro){
   
-           ## 3.1. leer archivo
-          
-          
-           ## 3.2. obtener codigo-DANE 
-          
-          
-           ## 3.3. obtener tipo de inversion
-          
-          
-           ## 3.4. obtener valor
-          
-          
-           ## 3.5. consolidar informacion 
-           
+            ## 3.1. leer archivo
+            
+            
+            ## 3.2. obtener codigo-DANE 
+            
+            
+            ## 3.3. obtener tipo de inversion
+            
+            
+            ## 3.4. obtener valor
+            
+            
+            ## 3.5. consolidar informacion 
+            
+            
+            ## 3.6 Retornar output
   
-## 3.6 Retornar output
-
 }
 
 
-
-  
-  
